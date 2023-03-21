e572335f204904ff7b0178b2197037f178918039
Matěj Oliva <matej.oliva@outlook.com>
2023-03-21 08:05:11 +0100

    [FIX] Deleted unnecessary code

--- src/api/categoryListRequests.js
+++ src/api/categoryListRequests.js
@@ -1,11 +1,6 @@
 export const categoryListRequest = async () => {
   const response = await fetch('https://api.chucknorris.io/jokes/categories')
     .then((response) => {
-      if (!response.ok) {
-        throw new Error(
-          `Failed to fetch random joke. Status: ${response.status}\nDetails: ${response.statusText}`,
-        );
-      }
       return response.json();
     })
     .then((data) => {
